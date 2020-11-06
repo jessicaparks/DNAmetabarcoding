810c2a0f64a88167483864de4c999c5070e46888
Jessica Parks <jparks@agbiome.com>
2020-11-06 12:29:51 -0500

    update the merging for taxonomy and abundance data
    using a different method of merging this data

--- DNAmetabarcoding/visualizations.py
+++ DNAmetabarcoding/visualizations.py
@@ -6,30 +6,75 @@ import pandas as pd
 
 
 def mergefiles(directory):
-    directory = directory + r"\*.csv"
+    #directory = directory + r"\*.csv"
+    files = glob.glob(f'{directory}/*.csv')
     #print(directory)
 
-    abundancefilelist = []
+    #abundancefilelist = []
     #print(type(abundancefilelist))
 
     #print(glob.glob(directory))
     #df = pd.DataFrame()
-    for counter, files in enumerate(glob.glob(directory)):
+    
+    # abundance data by sequence for each sample
+    abundance = (
+        pd.concat(
+            [
+                pd.read_csv(f)
+                .set_index('sequence')
+                [['abundance']]
+                .rename(columns={'abundance': f.rsplit('.', 1)[0]})
+                for f in files
+            ],
+            axis=1,
+            sort=False
+        )
+        .fillna(0)
+        .astype(int)
+        .reset_index()
+        .rename(columns={'index': 'sequence'})
+    )
+    abundance.to_csv('mergedASVdata.csv')
+    
+    # taxonomy classification data for each sequence
+    taxa = (
+        pd.concat(
+            [
+                pd.read_csv(f)
+                [['sequence', 'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']]
+                for f in files
+            ],
+            axis=0,
+            sort=False,
+            ignore_index=True
+        )
+        .drop_duplicates()
+        .reset_index(drop=True)
+    )
+    taxa.to_csv('mergedtaxadata.csv')
+    
+    # check the lengths of the abundance and taxa data are the same
+    # if not, this indicates that the same sequence was identified as
+    # belonging to different taxa, and some of the data should be updated
+    assert len(abundance) == len(taxa)
+
+    #for counter, files in enumerate(glob.glob(directory)):
         #print(counter)
-        if counter == 0:
-            taxafile = pd.read_csv(files, index_col = 0, usecols = [0,2,3,4,5,6,7,8])
-        else:
-            taxafile = taxafile.merge(pd.read_csv(files, index_col = 0, usecols = [0,2,3,4,5,6,7,8]), on = "sequence", how = 'outer')
+    #    if counter == 0:
+    #        taxafile = pd.read_csv(files, index_col = 0, usecols = [0,2,3,4,5,6,7,8])
+    #    else:
+    #        taxafile = taxafile.merge(pd.read_csv(files, index_col = 0, usecols = [0,2,3,4,5,6,7,8]), on = "sequence", how = 'outer')
             #missing_x = taxafile[taxafile['kingdom_x'].empty].index.tolist()
             #if taxafile.kingdom_x.to_string(index=False) == "" and taxafile.kingdom_y.to_string() 
             #print(missing_x)
 
             #I was attempting to do some type of filtering here in the comments to get it back to one set of columns but including the new data for each merge
-        abundancefilelist.append(pd.read_csv(files, index_col = 0, usecols = [0,1]))
+    #    abundancefilelist.append(pd.read_csv(files, index_col = 0, usecols = [0,1]))
     #print(taxafile)
-    abundancedataframe = pd.concat(abundancefilelist, axis = 1)
-    abundancedataframe.to_csv("mergedASVdata.csv")
-    taxafile.to_csv("mergedtaxadata.csv")
+    #abundancedataframe = pd.concat(abundancefilelist, axis = 1)
+    #abundancedataframe.to_csv("mergedASVdata.csv")
+    #taxafile.to_csv("mergedtaxadata.csv")
+    
 
 if __name__ == "__main__":
     location = sys.argv[1]
