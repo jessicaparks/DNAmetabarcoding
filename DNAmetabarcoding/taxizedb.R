d988f41e7e51cd4017d9b551192e27109fa70060
jessicaparks <jparks@agbiome.com>
2020-11-05 20:48:58 -0500

    add R script to run taxizedb for a list of tax ids

---
+++ DNAmetabarcoding/taxizedb.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+# set HOME environment variable to group directory because
+# the taxize database was downloaded to this location
+Sys.setenv(HOME = "/usr/local/usrapps/trnL_blast")
+
+# load libraries
+library(taxizedb, quietly=TRUE)
+library(data.table)
+
+# get the input and output file paths
+args <- commandArgs(trailingOnly = TRUE)
+file <- args[1]
+output <- args[2]
+
+# load the ncbi taxize db connection
+src <- src_ncbi()
+
+# read input data (list of taxids)
+data <- read.csv(file, header=FALSE, col.names="taxid")
+
+# taxonomic classification for each taxid
+results <- vector('list', length(data))
+i <- 1
+for (taxid in data$taxid){
+  result <- classification(taxid)[[1]]
+  result$taxid <- taxid
+  results[[i]] <- result
+  i <- i + 1
+}
+results <- rbindlist(results)
+
+# write results to output file
+write.csv(results, output, row.names=FALSE)
