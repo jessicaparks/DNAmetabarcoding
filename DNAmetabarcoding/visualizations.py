307bcb5d4835612615b8260c31588bfb7acc4146
Jessica Parks <jparks@agbiome.com>
2020-11-09 15:50:43 -0500

    pass output plot path to phyloseq

--- DNAmetabarcoding/visualizations.py
+++ DNAmetabarcoding/visualizations.py
@@ -10,17 +10,19 @@ import subprocess
 ranks = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
 
 
-def run_phyloseq(ASVfile, taxafile, taxalevel):
-    subprocess.check_call(['Rscript', 'phyloseq.R', ASVfile, taxafile, taxalevel])
+def run_phyloseq(asvfile, taxafile, taxalevel, plotfile):
+    subprocess.check_call(['Rscript', 'phyloseq.R', asvfile, taxafile, taxalevel, plotfile])
 
 
 @click.command()
 @click.option('-d', '--directory', required=True)
-@click.option('-o', '--output', required=True)
+@click.option('-o', '--outputplot', required=True)
 @click.option('-r', '--rank', type=click.Choice(ranks, case_sensitive=False), required=True)
-def main(directory, output, rank):
+def main(directory, outputplot, rank):
     files = glob.glob(f'{directory}/*.csv')
-    os.makedirs(output, exist_ok=True)
+    outputdir = os.dirname(outputplot)
+    outputname = os.basename(outputplot).rsplit('.', 1)[0]
+    os.makedirs(outputdir, exist_ok=True)
 
     # abundance data by sequence for each sample
     abundance = (
@@ -40,7 +42,7 @@ def main(directory, output, rank):
         .reset_index()
         .rename(columns={'index': 'sequence'})
     )
-    abundance_file = f'{output}/mergedASVdata.csv'
+    abundance_file = f'{outputdir}/{outputname}_ASV.csv'
     abundance.to_csv(abundance_file, index=False)
 
     # taxonomy classification data for each sequence
@@ -59,7 +61,7 @@ def main(directory, output, rank):
         .reset_index(drop=True)
 	.fillna('Unknown')
     )
-    taxa_file = f'{output}/mergedtaxadata.csv'
+    taxa_file = f'{output}/{outputname}_taxa.csv'
     taxa.to_csv(taxa_file, index=False)
 
     # check the lengths of the abundance and taxa data are the same
@@ -67,7 +69,7 @@ def main(directory, output, rank):
     # belonging to different taxa, and some of the data should be updated
     assert len(abundance) == len(taxa)
 
-    run_phyloseq(abundance_file, taxa_file, rank)
+    run_phyloseq(abundance_file, taxa_file, rank, outputplot)
 
 if __name__ == '__main__':
     main()
