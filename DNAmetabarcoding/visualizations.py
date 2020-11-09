1383f4078c6466677d07ec0b5d182b63d11cbac4
jessicaparks <jparks@agbiome.com>
2020-11-09 10:07:12 -0500

    update file so it'll run on cluster

--- DNAmetabarcoding/visualizations.py
+++ DNAmetabarcoding/visualizations.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
+
+import click
 import os
-import sys
 import glob
 import pandas as pd
 import subprocess
 
+# set options for taxonomic ranks
+ranks = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
+
+
 def run_phyloseq(ASVfile, taxafile, taxalevel):
     subprocess.check_call(['Rscript', 'phyloseq.R', ASVfile, taxafile, taxalevel])
-    
-def mergefiles(directory, taxalevel):
+
+
+@click.command()
+@click.option('-d', '--directory', required=True)
+@click.option('-r', '--rank', type=click.Choice(ranks, case_sensitive=False), required=True)
+def main(directory, rank):
     files = glob.glob(f'{directory}/*.csv')
 
     # abundance data by sequence for each sample
     abundance = (
         pd.concat(
             [
-                pd.read_csv(f)
+             	pd.read_csv(f)
                 .set_index('sequence')
                 [['abundance']]
                 .rename(columns={'abundance': f.rsplit('.', 1)[0]})
@@ -24,18 +33,18 @@ def mergefiles(directory, taxalevel):
             axis=1,
             sort=False
         )
-        .fillna(0)
+	.fillna(0)
         .astype(int)
         .reset_index()
         .rename(columns={'index': 'sequence'})
     )
     abundance.to_csv('mergedASVdata.csv')
-    
+
     # taxonomy classification data for each sequence
     taxa = (
-        pd.concat(
+	pd.concat(
             [
-                pd.read_csv(f)
+             	pd.read_csv(f)
                 [['sequence', 'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']]
                 for f in files
             ],
@@ -43,21 +52,18 @@ def mergefiles(directory, taxalevel):
             sort=False,
             ignore_index=True
         )
-        .drop_duplicates()
+	.drop_duplicates()
         .reset_index(drop=True)
     )
     taxa.to_csv('mergedtaxadata.csv')
-    
+
     # check the lengths of the abundance and taxa data are the same
     # if not, this indicates that the same sequence was identified as
     # belonging to different taxa, and some of the data should be updated
     assert len(abundance) == len(taxa)
 
-    run_phyloseq('mergedASVdata.csv', 'mergedtaxadata.csv', taxalevel)
-    
+    run_phyloseq('mergedASVdata.csv', 'mergedtaxadata.csv', rank)
 
-if __name__ == "__main__":
-    location = sys.argv[1]
-    taxalevel = sys.argv[2]
-    mergefiles(location, taxalevel)
+if __name__ == '__main__':
+    main()
 
