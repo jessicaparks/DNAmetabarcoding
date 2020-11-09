c9719bd10ddd4c754b14fad95f0541f2cedc8639
jessicaparks <jparks@agbiome.com>
2020-11-09 00:05:23 -0500

    remove commented-out code

--- DNAmetabarcoding/visualizations.py
+++ DNAmetabarcoding/visualizations.py
@@ -9,16 +9,8 @@ def run_phyloseq(ASVfile, taxafile, taxalevel):
     subprocess.check_call(['Rscript', 'phyloseq.R', ASVfile, taxafile, taxalevel])
     
 def mergefiles(directory, taxalevel):
-    #directory = directory + r"\*.csv"
     files = glob.glob(f'{directory}/*.csv')
-    #print(directory)
 
-    #abundancefilelist = []
-    #print(type(abundancefilelist))
-
-    #print(glob.glob(directory))
-    #df = pd.DataFrame()
-    
     # abundance data by sequence for each sample
     abundance = (
         pd.concat(
@@ -62,22 +54,6 @@ def mergefiles(directory, taxalevel):
     assert len(abundance) == len(taxa)
 
     run_phyloseq('mergedASVdata.csv', 'mergedtaxadata.csv', taxalevel)
-    #for counter, files in enumerate(glob.glob(directory)):
-        #print(counter)
-    #    if counter == 0:
-    #        taxafile = pd.read_csv(files, index_col = 0, usecols = [0,2,3,4,5,6,7,8])
-    #    else:
-    #        taxafile = taxafile.merge(pd.read_csv(files, index_col = 0, usecols = [0,2,3,4,5,6,7,8]), on = "sequence", how = 'outer')
-            #missing_x = taxafile[taxafile['kingdom_x'].empty].index.tolist()
-            #if taxafile.kingdom_x.to_string(index=False) == "" and taxafile.kingdom_y.to_string() 
-            #print(missing_x)
-
-            #I was attempting to do some type of filtering here in the comments to get it back to one set of columns but including the new data for each merge
-    #    abundancefilelist.append(pd.read_csv(files, index_col = 0, usecols = [0,1]))
-    #print(taxafile)
-    #abundancedataframe = pd.concat(abundancefilelist, axis = 1)
-    #abundancedataframe.to_csv("mergedASVdata.csv")
-    #taxafile.to_csv("mergedtaxadata.csv")
     
 
 if __name__ == "__main__":
