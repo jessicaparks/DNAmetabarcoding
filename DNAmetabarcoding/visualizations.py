e857dce146f1354cfa9d8cd28450fd8b57392e0b
jessicaparks <jparks@agbiome.com>
2020-11-10 14:17:23 -0500

    change tabs to spaces

--- DNAmetabarcoding/visualizations.py
+++ DNAmetabarcoding/visualizations.py
@@ -56,7 +56,7 @@ def main(directory, outputdir, outputprefix, rank, filter):
             axis=1,
             sort=False
         )
-	.fillna(0)
+        .fillna(0)
         .astype(int)
         .reset_index()
         .rename(columns={'index': 'sequence'})
@@ -67,7 +67,7 @@ def main(directory, outputdir, outputprefix, rank, filter):
 
     # taxonomy classification data for each sequence
     taxa = (
-	pd.concat(
+        pd.concat(
             [
              	pd.read_csv(f)
                 [['sequence', 'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']]
@@ -77,9 +77,9 @@ def main(directory, outputdir, outputprefix, rank, filter):
             sort=False,
             ignore_index=True
         )
-	.drop_duplicates()
+        .drop_duplicates()
         .reset_index(drop=True)
-	.fillna('Unknown')
+        .fillna('Unknown')
     )
     taxa_file = f'{outputdir}/{outputprefix}_taxa.csv'
     taxa.to_csv(taxa_file, index=False)
@@ -87,10 +87,10 @@ def main(directory, outputdir, outputprefix, rank, filter):
 
     # create a merged ASV and taxa file for all the samples
     all_data = pd.merge(
-	abundance,
-	taxa,
-	on='sequence',
-	how='inner'
+        abundance,
+        taxa,
+        on='sequence',
+        how='inner'
     )
     all_data_file = f'{outputdir}/{outputprefix}_all.csv'
     all_data.to_csv(all_data_file, index=False)
@@ -101,11 +101,11 @@ def main(directory, outputdir, outputprefix, rank, filter):
     count_remove = len(all_data[all_data[rank]!='Unknown'][rank].unique()) - filter
     print(f'{count_remove} {rank} values will be replaced by "Other".')
     keep = (
-	all_data[all_data[rank]!='Unknown']
-	.groupby(rank).sum()
-	.sum(axis=1)
-	.sort_values(ascending=False)[:20]
-	.index.tolist() + ['Unknown']
+        all_data[all_data[rank]!='Unknown']
+        .groupby(rank).sum()
+        .sum(axis=1)
+        .sort_values(ascending=False)[:20]
+        .index.tolist() + ['Unknown']
     )
     taxa[rank] = taxa[rank].apply(lambda x: x if x in keep else 'Other')
     
@@ -118,9 +118,11 @@ def main(directory, outputdir, outputprefix, rank, filter):
     # if not, this indicates that the same sequence was identified as
     # belonging to different taxa, and some of the data should be updated
     if len(abundance) != len(taxa):
-	raise ValueError('ASVs are not uniquely identified from all samples. '
-			 'Check that the samples were analyzed with the same taxonomy '
-			 'method (BLAST or DADA2) and the same database.')
+        raise ValueError(
+            'ASVs are not uniquely identified from all samples. '
+            'Check that the samples were analyzed with the same taxonomy '
+            'method (BLAST or DADA2) and the same database.'
+    )
 
     plot_file = f'{outputdir}/{outputprefix}_abundance_plot.png'
     run_phyloseq(abundance_file, filt_taxa_file, rank, plot_file)
